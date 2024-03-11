b475b9b6a9badaf790a04d56090176f938ad0886
Henry Faulkner <hfaulkner27@gmail.com>
2024-03-10 21:00:45 -0400

    initial site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faSquareXTwitter, faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChefKnifeStudios.com';
+  faSquareXTwitter = faSquareXTwitter;
+  faSquareInstagram = faSquareInstagram;
 }
